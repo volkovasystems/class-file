e02a7e8dbf320a86fd5f2a574fb49e1405f6eb9b
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-01-30 01:12:03 +0800

    Added class File.

---
+++ class-file.js
@@ -0,0 +1,49 @@
+/*:
+	@include:
+		{
+			"fs": "fs",
+			"check-if-empty": "checkIfEmpty",
+			"class-path": "Path",
+			"class-file": "File",
+			"subset": "subset",
+			"class-range": "Range"
+		}
+	@end-include
+
+	@export:
+		{
+			"parseCommands": "parseCommands"
+		}
+	@end-export
+*/
+File = function File( path ){
+
+};
+
+File.prototype.read = function read( ){
+
+};
+
+File.prototype.write = function write( data ){
+
+};
+
+File.prototype.decodeAs = function decodeAs( decoding ){
+
+};
+
+File.prototype.encodeAs = function encodeAs( encoding ){
+
+};
+
+File.prototype.watch = function watch( handler ){
+
+};
+
+File.prototype.destroy = function destroy( ){
+
+};
+
+File.prototype.getLocation = function getLocation( ){
+
+};
